7c2f3977d31e64d46c008c6bf2d6a57776e23337
yonybrinez <yony@blackrocket.co>
2020-07-30 22:08:41 -0500

    0. Start a script

---
+++ markdown2html.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+import os.path
+
+if len(sys.argv) < 2:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    sys.exit(1)
+else:
+    if os.path.isfile('./' + sys.argv[1]):
+        sys.exit()
+    else:
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        sys.exit(1)
+
